4a2db042c0ccc5ac83e67929219bee061600cfff
Marcin Madej <marcin.madej1993@gmail.com>
2019-10-22 21:54:17 +0200

    initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'ui-dark',
+            template: __dirname + '/src/index.html'
+        })
+    ]
+};
